9127da6e25a667d1cf934bf0e21c8a9d134387f1
Kavindi Piyumika <kavindipiyumika31@gmail.com>
2025-09-11 19:50:15 +0530

    add comment

--- sandbox.js
+++ sandbox.js
@@ -2,3 +2,6 @@ var character = 'Kavindi';
 console.log(character);
 var inputs = document.querySelectorAll('input');
 console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
--- sandbox.ts
+++ sandbox.ts
@@ -1,6 +1,12 @@
 const character = 'Kavindi';
+
 console.log(character);
 
 const inputs = document.querySelectorAll('input');
+
 console.log(inputs);
+
+inputs.forEach(input=>{
+    console.log(input);
+});
 
