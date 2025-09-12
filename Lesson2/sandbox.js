409cfc9997bafa4bcbd4866368d5143430fccae1
Kavindi Piyumika <kavindipiyumika31@gmail.com>
2025-09-12 13:38:53 +0530

    add the lesson2

---
+++ Lesson2/sandbox.js
@@ -0,0 +1,10 @@
+var character = 'Kavindi';
+var age = 30;
+var isBlackBelt = false;
+character = 'Piyumika';
+age = 20;
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(8.5));
---
+++ Lesson2/sandbox.ts
@@ -0,0 +1,18 @@
+let character = 'Kavindi';
+
+let age = 30 ;
+
+let isBlackBelt = false;
+
+character = 'Piyumika';
+
+age = 20 ;
+ 
+isBlackBelt = true;
+
+ const circ = (diameter:number) =>{
+    return diameter *Math.PI;
+ }
+
+ console.log(circ(8.5));
+ 
