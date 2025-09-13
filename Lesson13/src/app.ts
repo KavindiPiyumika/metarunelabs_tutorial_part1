24c0c51dceb1bf4e22ea6f84737328b9521bd575
Kavindi Piyumika <kavindipiyumika31@gmail.com>
2025-09-14 00:01:50 +0530

    Module

---
+++ Lesson13/src/app.ts
@@ -0,0 +1,38 @@
+import {Invoice} from './classes/invoice.js'
+
+//asign and call object
+const inv1 = new Invoice('mario' , 'work on the mario website' , 200);
+const inv2 = new  Invoice('Shaki' , 'work on the luigi website' , 300);
+
+
+//array
+let invoices:Invoice[] =[];
+invoices.push(inv1);
+invoices.push(inv2);
+
+invoices.forEach(inv =>{
+    console.log(inv.client ,inv.amount , inv.format());
+});
+
+
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;  //type casting
+console.log(form.children);
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toform = document.querySelector('#toform') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener ('submit',(e:Event) =>{
+e.preventDefault();
+
+console.log(
+    type.value,
+    toform.value,
+    details.value,
+    amount.value
+);
+
+})
